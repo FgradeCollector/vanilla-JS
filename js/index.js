1fa66d017851fc4e4e62c638ab63498547ce1812
FgradeCollector <sam940515@naver.com>
2023-03-05 20:27:45 +0900

    Update index.js
    확장자 변경

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
 const images = [
-    "0.png",
-    "1.png",
-    "2.png",
-    "3.png",
-    "4.png",
-    "5.png",
-    "6.png",
-    "7.png",
-    "8.png"
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg"
 ]
 
 const BODY = document.querySelector("body");
 function randomBackground() {
     const BGI = images[Math.floor(Math.random() * images.length)];
-    BODY.style.background = `${BGI}`;
 }
 
 randomBackground();

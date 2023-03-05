530419b81fa1e2bd979818f84e14fb40faa6f3e1
FgradeCollector <sam940515@naver.com>
2023-03-05 16:30:41 +0900

    Create geolocation.js

---
+++ js/geolocation.js
@@ -0,0 +1,20 @@
+const LOCATION = document.querySelector(".location");
+const API_KEY = "e49309bfb8d1d669d3438afb9dde5394";
+
+function geoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data);
+        const CITY = data.name;
+        const WEATHER = data.weather[0].main;
+        const TEMPERATURE = data.main.temp;
+        LOCATION.innerText = `${CITY} ${WEATHER} ${TEMPERATURE}°C`;
+    });
+}
+function geoError() {
+    LOCATION.innerText = `Error`;
+}
+
+setInterval(navigator.geolocation.getCurrentPosition(geoSuccess, geoError), 60000);

6fcafbb54cdd4be5af8fc880fc2899bf70632f37
FgradeCollector <sam940515@naver.com>
2023-03-06 03:30:07 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.querySelector(".to-do-form");
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector(".to-do-list");
 
+let toDos = [];
+
+function saveTodos() {
+    localStorage.setItem("todos",JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +29,17 @@ function toDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTodo);
     addToDo(newTodo);
+    saveTodos();
 }
 
 toDoForm.addEventListener("submit", toDoSubmit);
+
+const savedToDos = localStorage.getItem("todos");
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(localStorage.getItem("todos"));
+    toDos = parsedToDos;
+    parsedToDos.forEach(addToDo);
+}
